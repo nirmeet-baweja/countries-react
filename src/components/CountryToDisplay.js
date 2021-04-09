b7dd3ec063c8e72c0dbcab6b6abbb1c940c5959e
Nirmeet <nirmeet.baweja@gmail.com>
2021-04-09 13:37:31 +0100

    Amended search criteria to include country name as well its capital

--- src/components/CountryToDisplay.js
+++ src/components/CountryToDisplay.js
@@ -2,8 +2,10 @@ import React from "react";
 import CountryList from "./CountryList";
 
 const CountryToDisplay = ({ allCountries, searchValue }) => {
-  const filteredCountries = allCountries.filter((country) =>
-    country.name.toLowerCase().includes(searchValue.toLowerCase())
+  const filteredCountries = allCountries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+      country.capital.toLowerCase().includes(searchValue.toLowerCase())
   );
 
   return <CountryList countryList={filteredCountries} />;
